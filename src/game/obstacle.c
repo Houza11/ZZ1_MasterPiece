b831dde3cc0b6ae1b1abad6a0e0b62d14acfc5aa
Silver-Ink <martin.74@orange.fr>
2023-07-03 17:50:58 +0200

    premiers pattern créés

--- src/game/obstacle.c
+++ src/game/obstacle.c
@@ -1,4 +1,5 @@
 #include "base.h"
+#define tab_size(t) (sizeof(t)/sizeof(t[0]))
 
 
 void copy_pattern(game_arg arg, obstacle p[][EGG_NB_LIGNE], int size)
@@ -27,21 +28,52 @@ int pattern_add(game_arg arg, int id_pattern)
 {
     //Liste des patterns
     obstacle a[][EGG_NB_LIGNE] = {
-        {0,0,1,0,0},
-        {0,0,5,0,0},
-        {0,0,7,0,0},
-        {0,3,0,0,4}
-    }; int a_size = 4;
-    #define tab_size(t) (sizeof(t)/sizeof(t[0]))
-    printf("%d\n", a[0][2]);
-    printf("%d\n", tab_size(a));
+        { 0, 0, 0, 0, 0}
+    }; int a_size = 1;
     obstacle b[][EGG_NB_LIGNE] = {
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0}
+        {-1, 1, 1, 1, 1},
+        { 1, 0, 1, 1, 1},
+        { 1, 1, 0, 1, 1},
+        { 1, 1, 1, 0, 1},
+        { 1, 1, 1, 1,-2}
     }; int b_size = 5;
+    obstacle c[][EGG_NB_LIGNE] = {
+        { 1, 1,-1, 1, 1},
+        { 0, 0, 0, 0, 0},
+        { 1, 1, 1, 1, 0},
+        { 0, 0, 0, 0, 0},
+        { 1,-2, 1, 1, 1}
+    }; int c_size = 5;
+    obstacle d[][EGG_NB_LIGNE] = {
+        {-1,-1, 0, 0,-1},
+        { 0, 1, 0, 0, 1},
+        { 0, 1, 0, 0, 1},
+        { 0, 1, 0, 0, 1},
+        {-2, 1, 1, 1, 1}
+    }; int d_size = 5;
+    obstacle e[][EGG_NB_LIGNE] = {
+        {-1, 0,-1,-1,-1},
+        { 1, 0, 1, 0, 0},
+        { 1, 1, 1, 0, 0},
+        { 1, 0, 0, 1, 0},
+        { 1,-2,-2, 1, 1}
+    }; int e_size = 5;
+    obstacle f[][EGG_NB_LIGNE] = {
+        {-1,-1,-1,-1,-1},
+        { 0, 0, 1, 0, 0},
+        { 0, 1, 0, 0, 0},
+        { 0, 0, 0, 1, 0},
+        {-2,-2,-2,-2,-2}
+    }; int f_size = 5;
+    // obstacle e[][EGG_NB_LIGNE] = {
+    //     { 0, 0, 0, 0, 0},
+    //     { 0, 0, 0, 0, 0},
+    //     { 0, 0, 0, 0, 0},
+    //     { 0, 0, 0, 0, 0},
+    //     { 0, 0, 0, 0, 0}
+    // }; int e_size = 5;
+
+
 
     if (id_pattern < 0) { id_pattern = rand()%NB_PATTERN; }
     switch (id_pattern)
