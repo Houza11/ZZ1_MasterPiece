30709d36549603a6eaec4e35c76ccb2159de9037
Silver-Ink <martin.74@orange.fr>
2023-07-03 16:05:49 +0200

    ajout copy pattern

--- src/game/wall.c
+++ src/game/wall.c
@@ -1,21 +1,42 @@
 #include "base.h"
 
 
-
-void add_pattern()
+void copy_pattern(game_arg arg, obstacle** p, int size)
 {
-    obstacle** a = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {1, 1, 1, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
-    };
-    obstacle** a = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {1, 1, 1, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
-    };
+    for (int i = 0; i < size; i++)
+    {
+        
+    }
+    
+}
+
+#define NB_PATTERN 2
+void add_pattern(game_arg arg)
+{
+    //Liste des patterns
+    obstacle a[][5] = {
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    }; int a_size = 5;
+    obstacle b[][5] = {
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    }; int b_size = 5;
+
+    int rng = rand()%NB_PATTERN;
+    switch (rng)
+    {
+    case 0:
+        copy_pattern(arg, a, a_size);
+    break;
+    case 1:
+
+    break;
+    }
 }
