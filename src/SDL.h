a47891b86ac9fd1d56f1a66f2adc31bbc1ec43e5
Thomas-Mewily <tstd36@yahoo.fr>
2023-07-07 21:34:20 +0200

    show linux compilation command in sdl.h

--- src/SDL.h
+++ src/SDL.h
@@ -1,12 +1,13 @@
 #include "../include/SDL2/SDL.h"
 #include "../include/SDL2/SDL_image.h"
-#include "../include/SDL2/SDL_mixer.h"
+
+// Change the following include for :
 
 //#include <SDL2/SDL.h>
 //#include <SDL2/SDL_image.h>
+
+// compilation command :
 /*
 gcc -O4 -fdiagnostics-color=always -g ./src/*.c ./src/betterC/*.c ./src/context/*.c ./src/util/*.c ./src/collection/*.c ./src/scene/*.c ./src/ordi/*.c ./src/game/*.c -Wall -Wextra -Iinclude -Llib -lSDL2main -lSDL2 -lSDL2_image -lm -o ./bin/main.exe
 ./bin/main.exe
 */
-//#include "../include/SDL2/SDL_ttf.h"
-//#include <SDL2/SDL_ttf.h>
